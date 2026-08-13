607acb4b87bd3bb059d9c94a0b4a6a0d7add81ba
Monicke <monicke.oliveira@hotmail.com>
2026-08-13 14:55:22 -0300

    feat(about): create about section

---
+++ src/js/components/About.js
@@ -0,0 +1,18 @@
+export function renderAbout() {
+    return `
+        <section id="historia" class="about-section">
+            <h2 class="section-title">História</h2>
+            
+            <div class="about-container">
+                <div class="about-image">
+                    <img src="assets/images/picture.png" alt="Kauani, fundadora e confeiteira da Mimus Doces">
+                </div>
+                
+                <div class="about-text-card">
+                    <p class="highlight-text">Muito prazer, eu sou a Kauani.</p>
+                    <p>Minha história com a confeitaria começou aos 17 anos, quando descobri que fazer doces era a minha forma preferida de demonstrar afeto. Para mim, cada receita carrega um pouco de carinho. Foi essa paixão que transformou um sonho em realidade e fez nascer a Mimus Doces, um projeto que só existe hoje pelo apoio incondicional das pessoas que estão ao meu lado e me incentivam a criar momentos especiais para você.</p>
+                </div>
+            </div>
+        </section>
+    `;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import { renderHeader } from './components/Header.js';
 import { renderHero } from './components/Hero.js';
 import { renderMenu } from './components/Menu.js';
 import { renderModal } from './components/Modal.js';
+import { renderAbout } from './components/About.js';
+
 import { initMenu } from './utils/menu.js';
 import { initCarousel } from './utils/carousel.js';
 import { initModal } from './utils/modal.js';
@@ -13,6 +15,7 @@ app.innerHTML = `
     ${renderHero()}
     ${renderMenu()}
     ${renderModal()}
+    ${renderAbout()}
 `;
 
 initMenu();
